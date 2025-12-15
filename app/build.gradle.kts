5ff25e2c0d8f62e409cebfe77c8ca854fc1939bc
Your Name <you@example.com>
2025-12-14 19:11:55 -0800

    remove change minsdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.vayunmathur.crypto"
-        minSdk = 36
+        minSdk = 33
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
