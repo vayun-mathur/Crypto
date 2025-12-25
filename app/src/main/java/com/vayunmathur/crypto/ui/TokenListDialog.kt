a6caeeab6ebb03012fde0c4334e67368f4add02c
Your Name <you@example.com>
2025-12-25 12:36:14 +0800

    fix listing tokens

--- app/src/main/java/com/vayunmathur/crypto/ui/TokenListDialog.kt
+++ app/src/main/java/com/vayunmathur/crypto/ui/TokenListDialog.kt
@@ -1,10 +1,13 @@
 package com.vayunmathur.crypto.ui
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.vayunmathur.crypto.PortfolioViewModel
 import com.vayunmathur.crypto.token.JupiterLendRepository
 import com.vayunmathur.crypto.token.TokenInfo
@@ -13,18 +16,18 @@ import com.vayunmathur.crypto.token.TokenInfo
 fun TokenListDialog(tokenList: List<TokenInfo>, viewModel: PortfolioViewModel, onDismiss: () -> Unit) {
     AlertDialog(
         onDismissRequest = { onDismiss() },
-        title = { Text("Add a new Token to your portfolio") },
+        title = { Text("Add a new Token") },
         text = {
-            Column {
-                tokenList.forEach { tokenInfo ->
+            LazyColumn {
+                items(tokenList) { tokenInfo ->
                     val onClick = { viewModel.createTokenAccount(tokenInfo); onDismiss() }
                     if(tokenInfo.category == TokenInfo.Companion.Category.JUPITER_LEND) {
                         val apy = JupiterLendRepository[tokenInfo]?.apy ?: 0.0
-                        TextButton(onClick) {
+                        TextButton(onClick, Modifier.fillMaxWidth()) {
                             Text("${tokenInfo.name} - ${String.format("%.2f", apy * 100)}% APY")
                         }
                     } else {
-                        TextButton(onClick) {
+                        TextButton(onClick, Modifier.fillMaxWidth()) {
                             Text(tokenInfo.name)
                         }
                     }
